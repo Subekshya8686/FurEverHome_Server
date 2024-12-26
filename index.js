73bb5506cc5aeb285987d4421fa327b1ff8cb47f
Subekshya <subekshyakayastha4@gmail.com>
2024-12-26 17:03:23 +0545

    updated index

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoute = require("./route/userRoute");
+const petRoute = require("./route/petRoute");
+const formRoute = require("./route/formRoute");
+const adoptionRoute = require("./route/adoptionRoute");
+
 const app = express();
 
 connectDB();
 app.use(express.json());
 
-app.use("/user", userRoute);
+app.use("/api/user", userRoute);
+app.use("/api/pet", petRoute);
+app.use("/api", formRoute);
+app.use("/api", adoptionRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -18,6 +25,6 @@ app.post("/data", (req, res) => {
 });
 
 const PORT = 5000;
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
