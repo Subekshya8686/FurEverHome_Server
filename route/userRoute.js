e613550e318ab80a7252c8f1e7c4e1682d5d466d
Subekshya <subekshyakayastha4@gmail.com>
2025-01-04 15:22:03 +0545

    updated user routes

--- route/userRoute.js
+++ route/userRoute.js
@@ -5,15 +5,21 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  login,
+  uploadImage,
 } = require("../controller/userController");
+const UserValidation = require("../validation/userValidation");
+const upload = require("../config/multerConfig");
 
 const router = express.Router();
 
 // CRUD Routes
-router.post("/", createUser);          // Create a new user
-router.get("/", getUsers);             // Get all users
-router.get("/:id", getUserById);       // Get a specific user
-router.put("/:id", updateUser);        // Update an existing user
-router.delete("/:id", deleteUser);     // Delete a user
+router.post("/", createUser); // Create a new user
+router.post("/upload", upload.single("image"), uploadImage);
+router.get("/", getUsers); // Get all users
+router.get("/:id", getUserById); // Get a specific user
+router.put("/:id", updateUser); // Update an existing user
+router.delete("/:id", deleteUser); // Delete a user
+router.post("/login", login); // Login a user
 
 module.exports = router;
