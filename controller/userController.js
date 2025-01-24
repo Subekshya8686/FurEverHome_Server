fba1ffc670890fa369748e9846f1084c6ade72b9
Subekshya <subekshyakayastha4@gmail.com>
2025-01-24 12:03:52 +0545

    install and implement nodemailer to send email confirming registration

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,9 @@
 const User = require("../models/user");
-const bcrypt = require("bcryptjs"); // Fixed spelling of 'bcrypt'
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const upload = require("../config/multerConfig");
-// const user = require("../models/credential");
 const SECRET_KEY = "secretkey123";
+const nodemailer = require("nodemailer");
+const Pet = require("../models/pet");
 
 // Get all user
 const getUsers = async (req, res) => {
@@ -30,48 +30,6 @@ const getUserById = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch user" });
   }
 };
-
-// Create a new user
-// const createUser = async (req, res) => {
-//   try {
-//     const { name, email, password, phone, address, dateOfBirth, role } =
-//       req.body;
-//     const image = req.file ? req.file.filename : null;
-//     // Check if the email already exists
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       return res.status(400).json({ error: "Email already exists" });
-//     }
-
-//     // Hash the password
-//     const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
-
-//     // Create a new user with the hashed password
-//     const user = new User({
-//       name,
-//       email,
-//       password: hashedPassword, // Save the hashed password
-//       phone,
-//       image,
-//       address,
-//       dateOfBirth,
-//       role,
-//     });
-
-//     // Save the user to the database
-//     const savedUser = await user.save();
-
-//     // Return the saved user (excluding the password for security)
-//     const userResponse = savedUser.toObject();
-//     delete userResponse.password; // Remove the password from the response
-
-//     res.status(201).json(userResponse);
-//   } catch (error) {
-//     res
-//       .status(500)
-//       .json({ error: "Failed to create user", details: error.message });
-//   }
-// };
 
 const createUser = async (req, res) => {
   try {
@@ -102,12 +60,33 @@ const createUser = async (req, res) => {
     // Save to DB
     const savedUser = await user.save();
     const userResponse = savedUser.toObject();
-    delete userResponse.password; // Remove password from response for security
+    delete userResponse.password;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      protocol: "smtp",
+      auth: {
+        user: "subekshyakayastha4@gmail.com",
+        pass: "lshrfgtlqyjjdihl",
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: "subekshyakayastha4@gmail.com",
+      to: savedUser.email,
+      subject: "User Registration",
+      html: `
+      <h1>Your User Registration has been completed</h1>
+      `,
+    });
 
     // Send success response
     res.status(201).json({
       success: true,
       message: "User Created Successfully",
+      info,
     });
   } catch (error) {
     console.error(error); // Log the error for debugging
@@ -145,27 +124,6 @@ const deleteUser = async (req, res) => {
   }
 };
 
-// const login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Find the user by username
-//     const user = await User.findOne({ email });
-//     if (!user || !(await bcrypt.compare(password, user.password))) {
-//       return res.status(400).json({ error: "Invalid credentials" });
-//     }
-
-//     // Generate a JWT token
-//     const token = jwt.sign({ email, role: user.role }, SECRET_KEY, {
-//       expiresIn: "1h",
-//     });
-
-//     res.json({ message: "Login successful", token });
-//   } catch (error) {
-//     res.status(500).json({ error: "Failed to login", details: error.message });
-//   }
-// };
-
 const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -186,33 +144,23 @@ const login = async (req, res) => {
     const token = jwt.sign(
       { userId: user._id, email: user.email, role: user.role }, // Payload
       SECRET_KEY,
-      { expiresIn: "1h" } // Token expiration
+      { expiresIn: "6h" } // Token expiration
     );
 
     // Return success response with the token
-    res.json({ message: "Login successful", token });
+    res.json({
+      message: "Login successful",
+      token,
+      success: true,
+      role: user.role,
+      userId: user._id,
+    });
   } catch (error) {
     res.status(500).json({ error: "Failed to login", details: error.message });
   }
 };
 
 const uploadImage = async (req, res, next) => {
-  // Using multer middleware to handle the file upload
-  // upload(req, res, (err) => {
-  //   if (err) {
-  //     return res.status(400).json({ message: err.message }); // Handle errors like invalid file type or file size
-  //   }
-
-  //   if (!req.file) {
-  //     return res.status(400).json({ message: "Please upload a file" });
-  //   }
-
-  //   // Return success response with the uploaded file's filename
-  //   res.status(200).json({
-  //     success: true,
-  //     data: req.file.filename, // Send the file name as response
-  //   });
-  // });
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded." });
   }
@@ -221,6 +169,7 @@ const uploadImage = async (req, res, next) => {
     data: req.file.filename, // Send the file name as response
   });
 };
+
 
 module.exports = {
   getUsers,
