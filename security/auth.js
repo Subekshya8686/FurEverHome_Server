597ac085e1a6bdac131b337e99fb89307d43d93b
Subekshya <subekshyakayastha4@gmail.com>
2025-01-24 12:02:18 +0545

    fix authorization

--- security/auth.js
+++ security/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const SECRET_KEY = "secretkey123";
 
 function authenticateToken(req, res, next) {
-  const token = req.header("x-auth-token")?.split(" ")[1];
+  const token = req.header("Authorization")?.split(" ")[1];
   if (!token) return res.status(401).send("Access Denied");
 
   try {
@@ -14,12 +14,30 @@ function authenticateToken(req, res, next) {
   }
 }
 
+// function authorizeRole(role) {
+//   return (req, res, next) => {
+//     if (req.user.role !== role) {
+//       return res.status(403).send("Access Denied: Inssufficient Permissions");
+//     }
+//     next();
+//   };
+// }
+
 function authorizeRole(role) {
   return (req, res, next) => {
-    if (req.user.role !== role) {
-      return res.status(403).send("Access Denied: Inssufficient Permissions");
+    console.log(role);
+    console.log(req.user);
+    // Check if req.user exists and has the role property
+    if (!req.user || !req.user.role) {
+      return res.status(403).send("Access Denied: No Role Found");
     }
-    next();
+
+    // Check if the user has the required role
+    if (req.user.role !== role) {
+      return res.status(403).send("Access Denied: Insufficient Permissions");
+    }
+
+    next(); // User is authorized, proceed to the next middleware or route handler
   };
 }
 
